71e9dd7433c31efe404b78f4cac0623ca4f70c9d
James Roberts <james3roberts@gmail.com>
2024-09-22 15:30:32 -0600

    build: Trying to get everything to come together

--- Main.py
+++ Main.py
@@ -1,10 +1,4 @@
 import Measuring
 import Stepper
-
-def get_number():
-    cut_number = float(input('Enter a number less than 22.25: '))
-    while cut_number >= 22.25:
-        print("number must be less than 22.25")
-        cut_number = float(input('Enter a number less than 22.25: '))
-    return cut_number
+import time
 
