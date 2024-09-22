0cad00ea2b9bacda4872169478699c64fd0772cc
James Roberts <james3roberts@gmail.com>
2024-09-22 15:46:43 -0600

    Planning: make a list of what to include.

--- Main.py
+++ Main.py
@@ -2,3 +2,12 @@ import Measuring
 import Stepper
 import time
 
+"""First get the user to enter the distance they want it to move to. 
+we currenly have to stay between 5 and 16 inches.
+
+1. get the the distance from the user.
+2. run the measuring file to get the total distance.
+3. run the stepper file.
+4. keep checking with the measuring file.
+5. once the distance is achieved move back to start. 
+ """
